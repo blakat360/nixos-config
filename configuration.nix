c996651d53fea48fa87b9e6b577036538d17a11a
sigkill <blakat360@gmail.com>
2022-11-12 02:34:13 +0000

    steam works

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,8 @@
     grc
   ];
 
+  programs.steam.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
