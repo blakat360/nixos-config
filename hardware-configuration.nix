655bf21891a8ab8e365994d242694868c3dcc72b
sigkill <blakat360@gmail.com>
2022-11-25 00:09:35 +0000

    changed hardware.nix to match the autogened one

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,25 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/02e99672-9a75-42d3-b7fb-177efd48e29b";
+    { device = "/dev/disk/by-uuid/34114b34-7ba3-4c3c-999a-2675a8cd94cb";
       fsType = "ext4";
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/1A79-545C";
+    { device = "/dev/disk/by-uuid/E193-A0A5";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/d50edcbd-c8e7-4609-8290-0889f00e8ce4"; }
+    [ { device = "/dev/disk/by-uuid/963a4dbb-dfc7-4129-83d6-262e6bc2c00b"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -34,8 +32,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0f0.useDHCP = lib.mkDefault true;
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
