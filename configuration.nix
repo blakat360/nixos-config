77eae6258f7946b895332befbf6ad2b2f4d3a5a2
sigkill <blakat360@gmail.com>
2022-11-25 23:04:38 +0000

    added generation limit (5)

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,10 @@
 
   # Bootloader.
   boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 5;
+    };
     efi = {
       canTouchEfiVariables = true;
       efiSysMountPoint = "/boot/efi";
