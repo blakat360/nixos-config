9f1c54dc841baca4bbc6d7afd4100f85ba4ee823
sigkill <blakat360@gmail.com>
2023-01-08 01:23:45 +0000

    refactored rofi theming to make use of templating

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }:
 {
-  imports = [ ./theme.nix ];
-
   home.packages = with pkgs; [
     rofi-power-menu
   ];
@@ -9,5 +7,8 @@
   programs.rofi = {
     enable = true;
     font = "FiraCode Nerd Font 15";
+    theme = toString (pkgs.substituteAll (
+      { src = ./theme.rasi; } // config.colorScheme.colors
+    ));
   };
 }
