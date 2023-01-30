3439d857ac9f325dd7767871cbfde784c77f61e8
sigkill <blakat360@gmail.com>
2023-01-30 12:06:05 +0000

    added back scaling which i forgot cos im an idiot

--- home/personal-machine.nix
+++ home/personal-machine.nix
@@ -7,21 +7,31 @@
     ./st
   ];
 
-  home.packages = with pkgs; [
-    anki
-    bashmount
-    brightnessctl
-    pamixer
-    rofi-bluetooth
-    steam
-    vhs
-    xsel
-  ];
+  home = {
+    packages = with pkgs; [
+      anki
+      bashmount
+      brightnessctl
+      pamixer
+      rofi-bluetooth
+      steam
+      vhs
+      xsel
+    ];
+
+    sessionVariables = {
+      GDK_SCALE = 1;
+      GDK_DPI_SCALE = 0.75;
+      QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+      QT_AUTO_SCREEN_SET_FACTOR = 0;
+    };
+  };
+
 
   services = {
     betterlockscreen = {
       enable = true;
-      arguments = ["--color ${config.colorScheme.colors.base00}"];
+      arguments = [ "--color ${config.colorScheme.colors.base00}" ];
     };
     screen-locker = {
       enable = true;
