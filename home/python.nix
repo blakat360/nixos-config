2f978df43b69b58b96d04dd7b694206b0a980721
sigkill <blakat360@gmail.com>
2023-04-24 02:24:38 +0100

    added mkJupyterShell function which creates a shell.nix template for a jupyter notebook

--- home/python.nix
+++ home/python.nix
@@ -3,6 +3,31 @@
 {
   home.packages = with pkgs; [
     jupyter
-    # mach-nix
   ];
+
+  programs.fish.functions = {
+    mkJupyterShell =
+      let
+        shell_source = ''
+          with (import <nixpkgs> { });
+          let
+            mach-nix = import (builtins.fetchGit {
+              url = 'https://github.com/DavHau/mach-nix';
+              ref = 'refs/tags/3.5.0';
+            }) { };
+            built-in-reqs = builtins.readFile ./requirements.txt;
+            python = mach-nix.mkPython {
+              requirements = built-in-reqs + '''
+              ''';
+            };
+          in mach-nix.nixpkgs.mkShell {
+
+            buildInputs = [ python ];
+          }
+        '';
+      in
+      ''
+				echo "${shell_source}" > shell.nix
+      '';
+  };
 }
