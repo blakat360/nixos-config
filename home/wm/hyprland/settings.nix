c4fed1f7da208f43b2a481fa64c40aa9fb38d56d
sigkill <blakat360@gmail.com>
2024-04-15 22:04:11 +0100

    config is now workable

--- home/wm/hyprland/settings.nix
+++ home/wm/hyprland/settings.nix
@@ -13,14 +13,14 @@
     kb_layout = "us";
     kb_variant = " ";
     kb_model = " ";
-    kb_options = "caps:swapescape";
+    kb_options = "caps:escape";
     kb_rules = " ";
 
     follow_mouse = 1;
 
     touchpad =
       {
-        natural_scroll = false;
+        natural_scroll = true;
       };
 
     sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
