3c2394b74c0ea8c0f1c6bd6c75af225e1e3a6170
The default non-root user <blakat360@gmail.com>
2024-04-17 12:13:08 +0100

    improved cpp devx

--- home/languages/cpp.nix
+++ home/languages/cpp.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     cling
+    clang-tools
     cmake
     gcc
+    gnumake
     ninja
   ];
 
   xdg.configFile."clangd/config.yaml".text = ''
     CompileFlags:
       Add: [-std=c++20]
+    Diagnostics:
+      ClangTidy:
+        Add: *
   '';
 }
